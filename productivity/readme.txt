c3dedc0c33d162abddedf44c6818b0097a950e63
David Willison <david.a.willison1@gmail.com>
2021-11-17 14:57:24 -0600

    V2 - CSV printing feature still in development

---
+++ productivity/readme.txt
@@ -0,0 +1,11 @@
+This program takes a log file, reads it line by line and
+then prints formatted output that can be used for tracking
+work hours. 
+
+Basic program flow:
+
+Get file name
+Open file
+Read file and parse its data
+Convert the data into meaningful information
+Print out the user's selection of data formats
--- productivity/workLogParser.py
+++ productivity/workLogParser.py
@@ -1,51 +1,135 @@
 from datetime import timedelta
-     
-#startTime
-#breakTime
-#endTime
 
+
+#date      = d
+#startTime = s
+#breakTime = b
+#endTime   = e
+
+#d Nov 5 21
 #s 09:30
 #b 15
 #e 10:45
 
+def findTime(line):
+    """Converts text to the timedelta object"""
+    foundTime = line[1].split(":")
+    foundTime = timedelta(hours=int(foundTime[0]),minutes=int(foundTime[1]))
+    return foundTime
+
+
+def readStartEnd(log, line):
+    """
+    Reads the log line by line from a line starting with s
+    to a line starting with e and returns a dict with:
+    1. start time
+    2. shift length
+    """
+    startTime = timedelta(hours=0,minutes=0)
+    breakLen = timedelta(hours=0,minutes=0)
+    endTime = timedelta(hours=0,minutes=0)
+
+    # handles the start of a day
+    if(line[0] == "s"):
+        i = 0
+        startTime = findTime(line)
+        # limits the day to 10 starts, breaks, and ends
+        while(i < 10):
+            line = log.readline().split(" ")
+            if(line[0] == "b"):
+                breakLen += findTime(line)
+            elif(line[0] == "e"):
+                endTime = findTime(line)
+                break
+            i += 1
+
+        # does final calclations before returning the results
+        daytotal = (endTime - startTime) - breakLen
+        day = {'start':startTime,'total':daytotal}
+    return day
+
+
 def parseLog(fileName):
+    """
+    Opens log file for reading, determins the day, reads the 
+    contents of each day and puts it into its own day in a dict
+    """
     log = open(fileName, "r")
     line = log.readline().split(" ")
-    totalTime = timedelta(hours=0,minutes=0)
+    days = [{'start':timedelta(hours=0,minutes=0),'end':timedelta(hours=0,minutes=0),'total':timedelta(hours=0,minutes=0)}]
+    whichDay = 0
+    firstEntry = 0
 
+    # loops through the file until the end signaled by 'x'
     while(line[0] != "x"):
-        startTime = timedelta(hours=0,minutes=0)
-        breakLen = timedelta(hours=0,minutes=0)
-        endTime = timedelta(hours=0,minutes=0)
-        if(line[0] == "s"):
-            i = 0
-            startTime = line[1].split(":")
-            startTime = timedelta(hours=int(startTime[0]),minutes=int(startTime[1]))
-            while(i < 10):
-                line = log.readline().split(" ")
-                if(line[0] == "b"):
-                    breaktime = line[1].split(":")
-                    breaktime = timedelta(hours=int(breaktime[0]),minutes=int(breaktime[1]))
-                    breakLen = breakLen + breaktime
-                elif(line[0] == "e"):
-                    endTime = line[1].split(":")
-                    endTime = timedelta(hours=int(endTime[0]),minutes=int(endTime[1]))
-                    break
-                i += 1
-        timeLogged = (endTime - startTime) - breakLen
-        totalTime += timeLogged
-        print("- - - - - - - - - - - - - - - - -\nStart time:\t\t{}\nEnd time:\t\t{}\nTotal break time:\t{}\nTotal shift length:\t{}\n".format(str(startTime),str(endTime),str(breakLen),str(timeLogged)))
+        # handles a new day in the log
+        if(line[0] == "d"):
+            datestr = line[1] + " " + line[2] + ", " + line[3][:-1]
+            days.append({'date':datestr,'start':timedelta(hours=0,minutes=0),'end':timedelta(hours=0,minutes=0),'total':timedelta(hours=0,minutes=0)})
+            whichDay += 1
+            firstEntry = 0
+        # calls the function that reads through the contents of a day
+        # enters the contents into the variable containing parsed log
+        # file data
+        else:
+            startTotal = readStartEnd(log, line)
+            if firstEntry is 0:
+                days[whichDay]['start'] = startTotal['start']
+                firstEntry += 1
+            days[whichDay]['total'] += startTotal['total']
+            days[whichDay]['end'] = startTotal['start'] + startTotal['total']
         line = log.readline().split(" ")
+    
     log.close()
-    return totalTime
+    return days
+
+
+def csvMe(days,outputFileName):
+    """Still in development"""
+    pass
+
+
+def printing(days):
+    """Prints the printing options and handles each option"""
+    while True:
+        select = input("1 - Print results to csv file\n2 - List dates with time totals\n3 - List day, shift start, shift end, total time \n4 - Exit\nEnter the options you would like (e.g. 234)\n")
+
+        for opt in str(select):
+            # print to csv
+            if opt == "1":
+                outputFileName = input("Name for output file:")
+                csvMe(days,outputFileName)
+            tmp = days.pop(0)
+
+            # list | date   | time  |
+            if opt == "2":
+                print("| DATE\t\t| TOTAL\t\t|")
+                for i in days:
+                    print("| {}\t| {}\t|".format(i['date'], i['total']))
+                print("\n")
+
+            # list | date    | shift start  | shift end | total time    |
+            if opt == "3":
+                print("| DATE\t\t| START\t\t| END\t\t| TOTAL\t\t|")
+
+                for i in days:
+                    print("| {}\t| {}\t| {}\t| {}\t|".format(i['date'],i['start'],i['end'],i['total']))
+                print("\n")
+            days.insert(0,tmp)
+
+            if opt == "4":
+                select = "4"
+        
+        if select == "4":
+            break
 
 
 def main():
-    fileName = "workLog.txt"
-    totalTime = parseLog(fileName)
-    print("You worked {} hours this time period.".format(totalTime))
+    """Gets the file name and calls the funciton to parse it"""
+    fileName = input("Enter your log file's name:")
+    days = parseLog(fileName)
+    printing(days)
 
 
-  
 if __name__=="__main__":
     main()
